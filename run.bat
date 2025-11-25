c17afdab4e849a155b7ebe6f801b6c4573fe4653
ariel elkaslasi <099558er@gmail.com>
2025-11-25 18:52:36 +0200

    Add directory path resolution to BAT files credut to bil the goat

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
 REM Start Python script without console window and exit CMD
+cd /d "%~dp0"
 start "" pythonw src/main.py
 exit
--- run_dev.bat
+++ run_dev.bat
@@ -1,6 +1,7 @@
 @echo off
 title GPO Autofish - Development Mode
 color 0A
+cd /d "%~dp0"
 
 echo ========================================
 echo   GPO Autofish - Development Mode
